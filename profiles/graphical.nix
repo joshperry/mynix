55c84df526db400d0d0285621da8235140c4c0b5
Joshua Perry <josh@6bit.com>
2025-09-13 13:25:48 -0600

    Use gtk pinentry gui

--- profiles/graphical.nix
+++ profiles/graphical.nix
@@ -1,7 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./common.nix
   ];
+
+  programs.gnupg.agent.pinentryPackage = pkgs.pinentry.gtk2;
 
   security.pam.services.i3lock.enable = true;
   services.upower.enable = true;
