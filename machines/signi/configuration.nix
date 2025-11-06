55455e377919c470c5fbfaa0b2d1aeed81fb576c
Joshua Perry <josh@6bit.com>
2025-11-06 11:13:13 -0700

    Add soteria polkit agent ui to avoid gnome

--- machines/signi/configuration.nix
+++ machines/signi/configuration.nix
@@ -61,6 +61,7 @@
   #boot.blacklistedKernelModules = [ "nouveau" "nvidia" ];
 
   security.polkit.enable = true;
+  security.soteria.enable = true; # polkit auth agent
 
   networking.hostName = "signi"; # Define your hostname.
   networking.networkmanager.enable = true;
@@ -302,19 +303,6 @@
   ###
   # SERVICES
   systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
   };
 
   virtualisation = {
