318f1d1a74b0c733fc8106a00473b3ad251c5398
Ada <ada@6bit.com>
2026-03-01 10:24:36 -0700

    liver: add initialHashedPassword for josh
    Previously josh had no password set, preventing sudo access from the
    console. This matches the configuration on all other machines.
    
    Co-Authored-By: Joshua Perry <josh@6bit.com>

--- machines/liver/configuration.nix
+++ machines/liver/configuration.nix
@@ -59,6 +59,7 @@
   users.users.josh = {
     uid = 1000;
     group = "josh";
+    initialHashedPassword = "$6$rounds=3000000$plps8mAYoxl.ngM7$UICj9iFn3SvWEBmD6Zsv0pWu8fru2jGNqvXazc7BjM9CJJxCna.du8yytejQeAL9yjQ.943AXyv8fjgSxOX.4.";
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
